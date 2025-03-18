eaf7f0646c161c58b3ecb2fa42323971be54a626
stillness03 <stillness304@gmail.com>
2025-03-18 23:48:53 +0200

    models and forms for users was create (and add stule for font)

--- Goldgym/Goldgym/urls.py
+++ Goldgym/Goldgym/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('main.urls', namespace='main')),
+    path('user/', include('users.urls', namespace='user')),
 
 
 ]
